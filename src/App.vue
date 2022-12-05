f2a551541a826dbe389f277cea4a8c8819b07d4a
Diogo Alhada <93165102+Alhada06@users.noreply.github.com>
2022-12-05 16:46:30 +0000

    pretty simple solution

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
+<script setup>
+import { ref } from 'vue';
+
+const to = ref(null);
+const from = ref(null);
+</script>
 <template>
   <div class="w-full h-full flex justify-center items-center">
     <div>
@@ -6,15 +12,27 @@
           <img src="/assets/santa.svg" alt="" class="max-w-[12em]" />
           <div>
             <h1 class="font-serif text-4xl">Happy Holidays!</h1>
-            <div class="mt-4 bg-white text-black p-2 rounded">TO: John</div>
-            <div class="mt-2 bg-white text-black p-2 rounded">FROM: Joana</div>
+            <div class="mt-4 bg-white text-black p-2 rounded">TO: {{ to }}</div>
+            <div class="mt-2 bg-white text-black p-2 rounded">FROM: {{ from }}</div>
           </div>
         </div>
       </div>
+
+      <div class="mt-8">
+        <label class="block" for="to">
+          To: <input type="text" id="to" v-model="to"
+            class="rounded w-full border block  bg-gray-100 border-gray-300 focus:border-emerald-500 focus:ring-emerald-500 focus:ring p-1">
+        </label>
+      </div>
+      <div class="mt-8">
+        <label class="block" for="from">
+          From: <input id="from" type="text" v-model="from"
+            class="rounded w-full border block p-1  bg-gray-100 border-gray-300  focus:border-red-500 focus:ring-red-500 focus:ring">
+        </label>
+      </div>
     </div>
+
+
   </div>
 </template>
 
-<script setup>
-// ...
-</script>
